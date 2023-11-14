b442cede4f8343a85555a050bc753da4262c5148
tonk4atu <norimaki229@gmail.com>
2023-11-14 16:56:46 +0900

    ボタンのコンポーネント化

--- components/ContentButton.tsx
+++ components/ContentButton.tsx
@@ -4,31 +4,31 @@ import { useContentStore } from "@/app/store/store";
 const contentTab = () => {
   return (
     <div className="flex content-between">
-      <button
-        className=" hover:bg-gray-700"
-        value={"0"}
-        onClick={(e) => {
-          e.preventDefault();
-          clickHandler(e);
-        }}
-      >
-        コンテンツ0
-      </button>
-      <button
-        className=" hover:bg-gray-700"
-        value={"1"}
-        onClick={(e) => {
-          e.preventDefault();
-          clickHandler(e);
-        }}
-      >
-        コンテンツ1
-      </button>
+      <TabButton title={"コンテンツ0"} contentType={"0"} />
+      <TabButton title={"コンテンツ1"} contentType={"1"} />
     </div>
   );
 };
 export default contentTab;
 
-const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-  useContentStore.setState({ contentType: e.currentTarget.value });
+type TabButtonProps = {
+  title: string;
+  contentType: string;
+};
+
+const TabButton = (Props: TabButtonProps) => {
+  const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    useContentStore.setState({ contentType: e.currentTarget.value });
+  };
+  return (
+    <button
+      className="hover:bg-gray-800"
+      onClick={(e) => {
+        clickHandler(e);
+      }}
+      value={Props.contentType}
+    >
+      {Props.title}
+    </button>
+  );
 };
