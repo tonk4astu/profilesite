f69df19f6233ed388349ad62a7f670d38fab5900
tonk4atu <norimaki229@gmail.com>
2023-11-20 15:37:15 +0900

    Suspenseタグの追加

--- components/server/ContentContainer.tsx
+++ components/server/ContentContainer.tsx
@@ -2,13 +2,16 @@ import MusicContainer from "@/components/server/MusicContainer";
 import Content from "@/components/Contents";
 import ContentTab from "@/components/ContentButton";
 import LoLContainer from "@/components/lolContainer";
+import { Suspense } from "react";
 const ContentContainer = () => {
   return (
     <>
-      <ContentTab />
-      <Content id="0" Content={<>まだない</>} />
-      <Content id="1" Content={<MusicContainer />} />
-      <Content id="2" Content={<LoLContainer />} />
+      <Suspense fallback={<div>loading...</div>}>
+        <ContentTab />
+        <Content id="0" Content={<>まだない</>} />
+        <Content id="1" Content={<MusicContainer />} />
+        <Content id="2" Content={<LoLContainer />} />
+      </Suspense>
     </>
   );
 };
